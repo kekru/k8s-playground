c568e2afceb08cdc4a2131e81cf5abe0be6fe3fc
Kevin Krummenauer <lopusknut@googlemail.com>
2019-02-24 01:53:37 +0100

    nginx config to route backends based on sni name

---
+++ example/generic-svc-exposer-tcpsni/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+BASE_URL_ESCAPED=$(echo $BASE_URL | sed 's|\.|\\\\.|g')
+sed -i "s|BASE_URL|$BASE_URL_ESCAPED|g" /etc/nginx/nginx.conf
+sed -i "s|DNS_RESOLVER|$DNS_RESOLVER|g" /etc/nginx/nginx.conf
+
+exec nginx -g "daemon off;"
